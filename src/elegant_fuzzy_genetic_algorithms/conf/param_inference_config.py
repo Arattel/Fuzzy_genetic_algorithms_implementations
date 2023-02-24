da2f2b23e948207693ea121f4e0fcde47a98238a
Arattel <sizonov2000@gmail.com>
2023-02-24 18:00:34 +0200

    Implemented priority inference

--- src/elegant_fuzzy_genetic_algorithms/conf/param_inference_config.py
+++ src/elegant_fuzzy_genetic_algorithms/conf/param_inference_config.py
@@ -1,5 +1,11 @@
 from typing import Dict, Any
+from dataclasses import dataclass
+
 from src.elegant_fuzzy_genetic_algorithms.helpers.linguistic_variables import (init_x_rate, init_m_rate, init_subpop_size)
+
+@dataclass
+class Conf: 
+    priority_rules_path: str  = 'src/elegant_fuzzy_genetic_algorithms/rulesets/ruleset_priority_update.txt'
 
 parameters_to_infer: Dict[str, Any] = {
     'xRate': {

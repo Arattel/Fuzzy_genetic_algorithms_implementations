ced79cad1db5c4f46566940e1a1fd91f71543dbe
Arattel <sizonov2000@gmail.com>
2023-02-27 19:39:06 +0200

    Moved some stuff to configs

--- src/elegant_fuzzy_genetic_algorithms/conf/param_inference_config.py
+++ src/elegant_fuzzy_genetic_algorithms/conf/param_inference_config.py
@@ -1,23 +1,12 @@
 from typing import Dict, Any
 from dataclasses import dataclass
 
-# from src.elegant_fuzzy_genetic_algorithms.helpers.linguistic_variables import (init_x_rate, init_m_rate, init_subpop_size)
 
 @dataclass
 class Conf: 
-    priority_rules_path: str  = 'src/elegant_fuzzy_genetic_algorithms/rulesets/ruleset_priority_update.txt'
+    rule_path_param: str = 'src/elegant_fuzzy_genetic_algorithms/rulesets/ruleset_params.txt'
+    rule_path_priority: str = 'src/elegant_fuzzy_genetic_algorithms/rulesets/ruleset_priorityt.txt'
 
-# parameters_to_infer: Dict[str, Any] = {
-#     'xRate': {
-#     'fn': init_x_rate, 
-#     'path': 'src/elegant_fuzzy_genetic_algorithms/rulesets/ruleset_Xrate.txt'
-#     }, 
-#     'mRate': {
-#         'fn': init_m_rate, 
-#         'path': 'src/elegant_fuzzy_genetic_algorithms/rulesets/ruleset_mrate.txt'
-#     }, 
-#     'subPopSize': {
-#         'fn': init_subpop_size, 
-#         'path': 'src/elegant_fuzzy_genetic_algorithms/rulesets/ruleset_subpopsize.txt'
-#     }, 
-# }
+    default_params = {'xRate': 0.7,
+          'mRate': 0.025,
+          'subPopSize': 0.2}

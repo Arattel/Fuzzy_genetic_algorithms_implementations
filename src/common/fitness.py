f21f536e317bd8263141c4d86fb9cbe299f1d415
Arattel <sizonov2000@gmail.com>
2023-03-17 14:38:06 +0200

    More fitness functions, more experiments

--- src/common/fitness.py
+++ src/common/fitness.py
@@ -10,3 +10,17 @@ def schwefel(genome):
 def griewank(genome):
     return 1 + (np.sum(np.square(genome)) / 400) - np.product(np.cos(
         genome / (np.arange(genome.shape[0]) + 1)))
+
+
+def rastrigin(genome):
+    return np.sum(
+        np.square(genome) - 10 * np.cos(2*np.pi*genome) + 10
+    )
+
+def ackley(genome):
+    return -20 * np.exp(-0.2 * 
+                       np.sqrt(1 /  genome.shape[0]) * 
+                       np.sum(np.square(genome))) \
+                       - np.exp((1 / genome.shape[0]) * 
+                       np.sum(np.cos(2*np.pi * genome))) \
+                       + 20 + np.e

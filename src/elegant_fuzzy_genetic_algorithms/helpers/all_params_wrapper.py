e1ec6afe7996dd5b0a4ec7c522e22cabfe775323
Arattel <sizonov2000@gmail.com>
2023-03-30 22:24:47 +0300

    Wrapper with common api

---
+++ src/elegant_fuzzy_genetic_algorithms/helpers/all_params_wrapper.py
@@ -0,0 +1,18 @@
+from src.elegant_fuzzy_genetic_algorithms.helpers.generalized_param_inferrer import GeneralizedParamInferencer
+from src.elegant_fuzzy_genetic_algorithms.helpers.parallel_priority_wrapper import ParallelPriorityWrapper
+
+
+import multiprocessing as mp
+import numpy as np
+
+
+class AllEFGAParamsParallelWrapper:
+    def __init__(self, n_terms_params: int = 3, n_terms_priority: int = 3, n_processes: int = mp.cpu_count()) -> None:
+        self.priority = ParallelPriorityWrapper(n_terms_fitness=n_terms_priority, n_processes=n_processes)
+        self.params = GeneralizedParamInferencer(n_terms_params)
+    
+    def infer_priority(self, c1, c2):
+        return self.priority.infer_priority(c1, c2)
+    
+    def infer(self, bestFitness: float, avgFitness: float, avgFitChange: float) -> float:
+        return self.params.infer(bestFitness=bestFitness, avgFitness=avgFitness, avgFitChange=avgFitChange)
